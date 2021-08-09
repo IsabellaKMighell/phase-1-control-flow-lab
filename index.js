a42fe6fa28284f4c0032eff1e44912447f98b2bf
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-09 18:08:19 -0400

    commit tree 596d26ddc686313a0ea47ad5508b447c3b6c8636

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){
+  let msg;
+  if(feet<=400){
+    msg = 'This one is on me!';
+  }
+  else if(2000 < feet && feet< 2500){
+    msg = 'I will gladly take your thirty bucks.';
+  }
+  else if(feet>2500){
+    msg = 'No can do.';
+  }
+  return msg
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  const isNYC = city === "NYC"? "Ok, sounds good.": "No go.";
+  return isNYC;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let gratitude;
+
+  switch(tip){
+    case 'generous':
+      gratitude= "Thank you so much.";
+      break;
+    case 'not as generous':
+      gratitude = "Thank you.";
+      break;
+    default:
+      gratitude = 'Bye.';
+     }
+
+  return gratitude;
 }
